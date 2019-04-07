ee40ebd80beec3626136b6e2313921a8dfdb9273
propoch <propoch@gmail.com>
2019-04-07 10:09:09 +0700

    add new code

--- monitor_new_file.py
+++ monitor_new_file.py
@@ -5,15 +5,16 @@ from watchdog.events import PatternMatchingEventHandler
 import mysql.connector
 import configparser
 import logging
-
+import datetime
 class MyHandler(PatternMatchingEventHandler):
     patterns=["*.avi"]
     def process(self, event):
         if(event.event_type == "created"):
             prefix_file = str(datetime.date.today())
-            file_log = "%s%s-%s" % (config[ENV]['PATH_LOG'],prefix_file,"watch.log")
-            logging.basicConfig(filename=file_log, format='%(asctime)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S')
-            message = "Found new file:%s" % event.src_path
+            file_log = "%s/monitor/%s-%s" % (config[ENV]['PATH_LOG'],prefix_file,"watch.log")
+            logging.basicConfig(filename=file_log,level=logging.DEBUG, format='%(asctime)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S')
+            path_file = str(event.src_path).replace('../','/var/www/html/')
+            message = "Found new file:%s" % path_file
             logging.info(message)
             try:
                 mydb = mysql.connector.connect(
@@ -23,7 +24,7 @@ class MyHandler(PatternMatchingEventHandler):
                     database=config[ENV]["DB_DATABASE"])
                 try:
                     mycursor = mydb.cursor()
-                    path_file = event.src_path
+                    
                     sql = "INSERT INTO tbl_watch (watch_src_file, watch_hash,watch_created,watch_status) VALUES ('%s', '%s',now(),'Found')" % (path_file,hash(path_file))
                     mycursor.execute(sql)
                     mydb.commit()
@@ -55,6 +56,7 @@ if __name__ == '__main__':
     config.read('config.ini')
     ENV = config['DEFAULT']['ENV']
     observer = Observer()
+    print config[ENV]['PATH_MONITOR']
     observer.schedule(MyHandler(), path=config[ENV]['PATH_MONITOR'],recursive=True)
     observer.start()
     try:
