8ef7969df786421a2749861de56a1fd1f6e7ddb4
propoch <propoch@gmail.com>
2019-05-11 09:03:37 +0700

    add convert by slot

---
+++ run.sh
@@ -0,0 +1,11 @@
+cd /opt/monitorNewFileConvertToMP4
+python monitor_convert_file.py 0 &
+python monitor_convert_file.py 1 &
+python monitor_convert_file.py 2 &
+python monitor_convert_file.py 3 &
+python monitor_convert_file.py 4 &
+python monitor_convert_file.py 5 &
+python monitor_convert_file.py 6 &
+python monitor_convert_file.py 7 &
+python monitor_convert_file.py 8 &
+python monitor_convert_file.py 9 &
